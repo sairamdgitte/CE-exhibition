a685999520c68f83dac34eaf0fb7cf052fe65c93
sairamdgitte <sairamdgitte@gmail.com>
2023-09-28 16:16:06 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function redirectToPage(pageName) {
+    // Customize the redirection URL based on the passed parameter
+    const redirectTo = pageName + '.html';
+    window.location.href = redirectTo;
+}
+
